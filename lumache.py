8651561adc45a7a3f057a31874b62fc9ca271415
Ayjent <121435895+Ayjent@users.noreply.github.com>
2022-12-26 13:16:30 -0500

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -1,23 +1,8 @@
 """
-Schwurve - Python library for cooks and food lovers.
+Schwurve - Blender add-on.
 """
 
 __version__ = "0.9.1"
 
 
-class InvalidKindError(Exception):
-    """Raised if the kind is invalid."""
-    pass
 
-
-def get_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
